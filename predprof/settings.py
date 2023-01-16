19f316f7cb2d25e186fc25d24b5920c48fa0d7e9
Morzan6 <112253494+Morzan6@users.noreply.github.com>
2023-01-16 21:24:51 +0000

    modified:   predprof/settings.py

--- predprof/settings.py
+++ predprof/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+os.environ['DJANGO_SETTINGS_MODULE']='predprof.settings'
