2f5d81d55a13f5b36397258b19b39f14dd8a27bd
nazrom <122815621+nazrom@users.noreply.github.com>
2023-01-24 18:52:13 +0000

    modified:   scripts/handler.py

--- scripts/handler.py
+++ scripts/handler.py
@@ -1,6 +1,17 @@
+"""
+Чтобы запустить скрипт в терминале прописать "python  scripts/handler.py" (без кавычек)
+"""
 
-from services_model.models import *
+#просто настройки джанги
+import os, django
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
+#импорт модели сервиса
+from services_model.models import Service
 
-Dict = Service.status.values("name", "url")
+#полчаем массив со словарями в переменной Dict формата [{'name': 'МФТИ', 'url': 'https://mipt.ru/'}, {'name': 'МГТУ им. Н. Э. Баумана', 'url': 'https://bmstu.ru/'}...]
+Dict = Service.objects.values("name", "url")
 print(Dict)
