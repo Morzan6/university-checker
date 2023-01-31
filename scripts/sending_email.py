8663fb28a85ff4e70f459681247acf37c2eb1923
SleEpyHeDgEhOg6 <122866844+SleEpyHeDgEhOg6@users.noreply.github.com>
2023-01-31 20:16:58 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	new file:   scripts/sending_email.py

---
+++ scripts/sending_email.py
@@ -0,0 +1,21 @@
+import os, django
+import sys 
+sys.path.append('../../')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+
+
+from services_model.models import Service 
+from user_model.models import User
+from django.core.mail import EmailMessage
+
+def email_alert(service_slug,Error):
+    users = User.objects.filter( subscribes__icontains = service_slug )
+    service = Service.objects.get(slug = service_slug)
+    print(service.name)
+    for user in users:
+
+        email = EmailMessage(f'{service.name}-Сбой в работе сервиса {service.url}', f'Внимание замечен сбой в {service.url} ', to=[user.email]) #Написать более объемное предложение
+email_alert('mpti',200)        
