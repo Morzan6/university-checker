becc9cf6bb29d6593b387e254b365beeb1421431
Morzan6 <112253494+Morzan6@users.noreply.github.com>
2023-02-12 22:31:04 +0000

    modified:   config/settings.py 	deleted:    gunicorn/empty 	deleted:    install.sh 	deleted:    nginx/site.conf 	deleted:    systemd/gunicorn.service 	new file:   uwsgi/production.ini

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rc@04_mry_3-$@2sq$b9%-9jp6q2eyxf4bsw9&&esj++aw&r)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['university-checker.ru']
 
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
