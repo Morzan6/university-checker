52920f49c5e3aa73e0f64cfb70ec6bf6fef745e0
crazyBear2002 <122927836+crazyBear2002@users.noreply.github.com>
2023-02-14 20:01:04 +0000

    modified:   university-checker/scripts/time_delta.py

--- university-checker/scripts/time_delta.py
+++ university-checker/scripts/time_delta.py
@@ -19,38 +19,38 @@ def time_dif(ind):
 
     if (dif // 60) < 1:
         if dif == 11 or dif == 12 or dif == 13 or dif == 14:
-            return(str(dif, 'секунд назад'))
+            return(str(dif) + 'секунд назад')
         elif dif % 10 == 2 or dif % 10 == 3 or dif % 10 == 4:
-            return(str(dif, 'секунды назад'))
+            return(str(dif) + 'секунды назад')
         elif dif % 10 == 1:
-            return(str(dif, 'секунду назад'))
+            return(str(dif) + 'секунду назад')
         else:
-            return(str(dif, 'секунд назад'))
+            return(str(dif) + 'секунд назад')
     elif (dif // 3600) < 1:
         if dif // 60 == 11 or dif // 60 == 12 or dif // 60 == 13 or dif // 60 == 14:
-            return(str(dif // 60, 'минут назад'))
+            return(str(dif // 60) + 'минут назад')
         elif dif // 60 % 10 == 2 or dif // 60 % 10 == 3 or dif // 60 % 10 == 4:
-            return(str(dif // 60, 'минуты назад'))
+            return(str(dif // 60) + 'минуты назад')
         elif dif // 60 % 10 == 1:
-            return(str(dif // 60, 'минуту назад'))
+            return(str(dif // 60) + 'минуту назад')
         else:
-            return(str(dif // 60, 'минут назад'))
+            return(str(dif // 60) + 'минут назад')
     elif (dif // 86400) < 1:
         if dif // 3600 == 11 or dif // 3600 == 12 or dif // 3600 == 13 or dif // 3600 == 14:
-            return(str(dif // 3600, 'часов назад'))
+            return(str(dif // 3600) + 'часов назад')
         elif dif // 3600 % 10 == 2 or dif // 3600 % 10 == 3 or dif // 3600 % 10 == 4:
-            return(str(dif // 3600, 'часа назад'))
+            return(str(dif // 3600) + 'часа назад')
         elif dif // 3600 % 10 == 1:
-            return(str(dif // 3600, 'час назад'))
+            return(str(dif // 3600) + 'час назад')
         else:
-            return(str(dif // 3600, 'часов назад'))
+            return(str(dif // 3600) + 'часов назад')
     else:
         if dif // 86400 == 11 or dif // 86400 == 12 or dif // 86400 == 13 or dif // 86400 == 14:
-            return(str(dif // 86400, 'дней назад'))
+            return(str(dif // 86400) + 'дней назад')
         elif dif // 86400 % 10 == 2 or dif // 86400 % 10 == 3 or dif // 86400 % 10 == 4:
-            return(str(dif // 86400, 'дня назад'))
+            return(str(dif // 86400) + 'дня назад')
         elif dif // 86400 % 10 == 1:
-            return(str(dif // 86400, 'день назад'))
+            return(str(dif // 86400) + 'день назад')
         else:
-            return(str(dif // 86400, 'дней назад'))
+            return(str(dif // 86400) + 'дней назад')
 
