23c5ecd3327bdbbf177c078e6c9f7a3459bc33f9
Morzan6 <112253494+Morzan6@users.noreply.github.com>
2023-02-17 21:13:28 +0000

    modified:   university-checker/db.sqlite3 	new file:   university-checker/media/services_images/msu.png 	modified:   university-checker/scripts/handle_image.py

--- university-checker/scripts/handle_image.py
+++ university-checker/scripts/handle_image.py
@@ -1,6 +1,6 @@
 #обработчик изображений, который принимеат файл и сохраняет куда надшо
 
 def handle_uploaded_file(f, slug):
-    with open(f'media/services_images/{slug}.png', 'wb+') as destination:
+    with open(rf'university-checker/media/services_images/{slug}.png', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
